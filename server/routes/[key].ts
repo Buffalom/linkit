c0bb8d307ab4141133f30b7fbec2aebf1012f7d8
Tom Diggelmann <mail@tomdgm.ch>
2023-10-04 21:32:35 +0200

    add basic redirector server route with kv storage

---
+++ server/routes/[key].ts
@@ -0,0 +1,31 @@
+import type { H3Event } from 'h3'
+import { kv } from '@vercel/kv'
+
+interface Link {
+  url: string
+  calls?: number
+  latestCall?: string
+}
+
+export default defineEventHandler(async (event: H3Event) => {
+  const key = getRouterParam(event, 'key')
+
+  const link = await kv.hgetall<Partial<Link>>(key)
+
+  if (link?.url) {
+    await sendRedirect(event, link.url, 302)
+
+    await kv.hset(key, {
+      calls: (link.calls || 0) + 1,
+      latestCall: new Date().toISOString(),
+    })
+
+    return { link }
+  }
+
+  console.log(`Link with key '${key}' not found`)
+
+  await sendRedirect(event, '/c/p', 302)
+
+  return { link }
+})

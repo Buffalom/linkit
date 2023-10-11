47e6630cf31cc30ac8d34f21ad8e6d72d2edb6d2
Tom Diggelmann <mail@tomdgm.ch>
2023-10-11 23:53:45 +0200

    display link calls

--- pages/c/p/index.vue
+++ pages/c/p/index.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="mx-auto w-full max-w-xl px-4">
+  <div class="mx-auto w-full max-w-3xl px-4">
     <div class="rounded-2xl bg-gradient-to-tr from-violet-600 to-red-400 p-1">
       <div class="h-80 space-y-8 rounded-xl bg-white p-6">
         <h1 class="text-4xl font-bold">Links</h1>
 
-        <ul v-if="data">
+        <ul v-if="data" class="space-y-2">
           <li v-for="link in data.data" :key="link.key">
             <a
               :href="`/${link.key}`"
@@ -19,6 +19,13 @@
               class="text-blue-600 underline hover:text-blue-700 hover:no-underline"
               >{{ link.url }}</a
             >
+
+            <br />
+
+            <template v-if="'calls' in link && link.calls">
+              ({{ link.calls }} calls, last: {{ link.lastCall ?? 'unknown' }})
+            </template>
+            <template v-else> (0 calls) </template>
           </li>
         </ul>
       </div>
